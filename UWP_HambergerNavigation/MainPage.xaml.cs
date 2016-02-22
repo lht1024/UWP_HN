49a6774fca7ee60da098238d929b75181316b2ad
Li Hanyu <l1275561364@163.com>
2016-02-22 22:37:01 +0800

    initial commit

---
+++ UWP_HambergerNavigation/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace UWP_HambergerNavigation
+{
+
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MysplitView.IsPaneOpen = !MysplitView.IsPaneOpen;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TimeItem.IsSelected)
+            {
+                MyContent.Text = "Time";
+            }
+            else if (MessageItem.IsSelected)
+            {
+                MyContent.Text = "Message";
+            }
+            else if (RecordItem.IsSelected)
+            {
+                MyContent.Text = "Record";
+            }
+        }
+    }
+}
